a09382684b4eb1bb1bcea6d99291d8807a59c791
MalviRaval <malviraval18@gmail.com>
2025-03-06 12:24:52 +0530

    form screen design

--- lib/Screens/form_screen.dart
+++ lib/Screens/form_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class FormScreen extends StatelessWidget {
@@ -5,6 +7,39 @@ class FormScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final TextEditingController _titleController = TextEditingController();
+    final TextEditingController _descriptionController =
+        TextEditingController();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Form Screen'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          spacing: 16,
+          children: [
+            TextField(
+              controller: _titleController,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), hintText: 'Title'),
+            ),
+            TextField(
+              controller: _descriptionController,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), hintText: 'Description'),
+            ),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text('Submit'),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
