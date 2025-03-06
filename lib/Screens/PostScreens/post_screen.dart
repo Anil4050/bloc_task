4285dbab51a49f4fde88e5b9d02d1c2274432d90
Anil Gujariya <60058353+Anil4050@users.noreply.github.com>
2025-03-06 11:07:01 +0530

    initial API calling

--- lib/Screens/PostScreens/post_screen.dart
+++ lib/Screens/PostScreens/post_screen.dart
@@ -5,6 +5,56 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../Bloc/PostBloc/post_bloc.dart';
 
 
+
+
+// class PostScreen extends StatefulWidget {
+//   const PostScreen({super.key});
+
+//   @override
+//   State<PostScreen> createState() => _PostScreenState();
+// }
+
+// class _PostScreenState extends State<PostScreen> {
+//   @override
+//   void initState() {
+//     super.initState();
+//     context.read<PostBloc>().add(FetchPost());
+//   }
+//   @override
+//   Widget build(BuildContext context) {
+
+//         return Scaffold(
+//       appBar: AppBar(title: Text('Posts List')),
+//       body: BlocBuilder<PostBloc, PostState>(
+//         builder: (context, state) {
+//           if (state is PostLoading) {
+//             return Center(child: CircularProgressIndicator());
+//           } else if (state is PostLoaded) {
+//             return ListView.builder(
+//               itemCount: state.posts.length,
+//               itemBuilder: (context, index) {
+//                 return ListTile(
+//                   title: Text(state.posts[index]['title']),
+//                   subtitle: Text(state.posts[index]['body']),
+//                 );
+//               },
+//             );
+//           } else if (state is PostError) {
+//             return Center(child: Text(state.message));
+//           }
+//           return Center(child: Text('Press the button to fetch posts.'));
+//         },
+//       ),
+//       // floatingActionButton: FloatingActionButton(
+//       //   onPressed: () {
+//       //     context.read<PostBloc>().add(FetchPost()); // Trigger API call
+//       //   },
+//       //   child: Icon(Icons.cloud_download),
+//       // ),
+//     );
+//   }
+// }
+
 class PostScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -30,12 +80,12 @@ class PostScreen extends StatelessWidget {
           return Center(child: Text('Press the button to fetch posts.'));
         },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          context.read<PostBloc>().add(FetchPost()); // Trigger API call
-        },
-        child: Icon(Icons.cloud_download),
-      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () {
+      //     context.read<PostBloc>().add(FetchPost()); // Trigger API call
+      //   },
+      //   child: Icon(Icons.cloud_download),
+      // ),
     );
   }
 }
